8d491168f8671d23a301eebec3c8b326994005de
banban555 <ban_ban_55@naver.com>
2023-06-10 22:51:41 +0900

    feat:credentials true 삭제

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://3plus.world",
-  withCredentials: true,
 });
 
 export default api;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -25,7 +25,7 @@ const SignIn = () => {
       password: data.get("password"),
     };
     api
-      .post("/signin", userInfo, { withCredentials: true })
+      .post("/signin", userInfo)
       .then((res) => {
         if (res.data.loginSuccess === false) {
           setModalVisible(true);
