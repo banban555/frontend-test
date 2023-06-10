ec245ba430b9175dfb9c5916d5a0f58b975f05e0
banban555 <ban_ban_55@naver.com>
2023-06-10 20:04:04 +0900

    feat:쿠키 새엇 안되는 문제 해결

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -24,7 +24,7 @@ const SignIn = () => {
       password: data.get("password"),
     };
     api
-      .post("/signin", userInfo)
+      .post("/signin", userInfo, { withCredentials: true })
       .then((res) => {
         if (res.data.loginSuccess === false) {
           setModalVisible(true);
