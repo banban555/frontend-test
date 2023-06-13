7e6af9ca6bcd4d8ed464a3d2213c4c9aa21d072c
Yunseo Lee <112849440+lys3269@users.noreply.github.com>
2023-06-13 19:35:05 +0900

    fix : Update ModifyUserInfo.js 12

--- src/components/ModifyUserInfo.js
+++ src/components/ModifyUserInfo.js
@@ -35,13 +35,7 @@ const ModifyButton = styled.button`
 
 const ModifyUserInfo = () => {
   const token = window.localStorage.getItem("token");
-  const [userInfo, setUserInfo] = useState({
-    name: "",
-    studentNum: "",
-    email: "",
-    major: "",
-    grade: "",
-  });
+ 
   useEffect(() => {
     api
       .get("/application/userInfo",{ params: { token: token } })
@@ -57,6 +51,13 @@ const ModifyUserInfo = () => {
       });
   }, []);
   const [isModalVisible, setIsModalVisible] = useState(false); // 모달 visible state
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    studentNum: "",
+    email: "",
+    major: "",
+    grade: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -64,8 +65,7 @@ const ModifyUserInfo = () => {
   };
 
   const handleSubmit = (e) => {
-  
-
+    e.preventDefault();
     // 서버로 회원정보 수정 요청 보내기
     const data = {
       name: userInfo.name,
